4bbb28fbba728be1f652f1f57bbde8ef96eb69d3
Laurent NOPOLY <laurentnopoly@gmail.com>
2023-05-11 13:58:47 +0200

    Flask x ML

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, render_template
+from test_model import model
 
 app = Flask(__name__)
 
@@ -13,7 +14,8 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit_form():
     X = [request.form['squareMeters'],request.form['numberOfRooms'],request.form['floors'],request.form['cityCode'],request.form['cityPartRange'],request.form['numPrevOwners'],request.form['made'],request.form['basement'],request.form['attic'],request.form['garage']]
-    return f'Hello, {X}!'
+    result = model(X)
+    return f'Hello, {result}!'
 
 
 if __name__ == '__main__':
