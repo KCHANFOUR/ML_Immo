3b436aa4bffa0eba0d1db3d759cf48a7bc330b7c
Laurent NOPOLY <laurentnopoly@gmail.com>
2023-05-11 11:08:09 +0200

    Features tab

--- flaskr/app.py
+++ flaskr/app.py
@@ -4,14 +4,16 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+X = []
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    name = request.form['name']
-    return f'Hello, {name}!'
+    X = [request.form['squareMeters'],request.form['numberOfRooms'],request.form['floors'],request.form['cityCode'],request.form['cityPartRange'],request.form['numPrevOwners'],request.form['made'],request.form['basement'],request.form['attic'],request.form['garage']]
+    return f'Hello, {X}!'
 
 
 if __name__ == '__main__':
