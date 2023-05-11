85b697d7ce6e1882cd8a4d4fa058b3e20bb99e9c
Laurent NOPOLY <laurentnopoly@gmail.com>
2023-05-11 14:31:56 +0200

    reset flaskr app

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, render_template
-from test_model import model
 
 app = Flask(__name__)
 
@@ -14,8 +13,7 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit_form():
     X = [request.form['squareMeters'],request.form['numberOfRooms'],request.form['floors'],request.form['cityCode'],request.form['cityPartRange'],request.form['numPrevOwners'],request.form['made'],request.form['basement'],request.form['attic'],request.form['garage']]
-    result = model(X)
-    return f'Hello, {result}!'
+    return f'Hello, {X}!'
 
 
 if __name__ == '__main__':
