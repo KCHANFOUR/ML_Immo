810f67755456cccedf380661151b81975c4683ad
Laurent NOPOLY <laurentnopoly@gmail.com>
2023-05-16 17:29:08 +0200

    Change repository structure

--- Main.py
+++
@@ -1,8 +0,0 @@
-from preprocess import X_train, X_test, y_train, y_test, X_transform, y
-from flaskr import app
-import test_model
-
-if __name__=="__main__":
-    print("Calculate result")
-    app.app.run()
-
